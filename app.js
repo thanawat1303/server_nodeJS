ecbbe0c8f16b25f922b32b9ee47e0f14f1dc175a
thanawat1303 <fquarius13@gmail.com>
2023-01-16 14:27:58 +0700

    1

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 let port = '8000'
 
 app.set('view engine' , 'ejs')
 app.set('views' , 'view')
+app.use(express.static('assets'))
 
 app.get('/' , (req , res)=>{
-    res.render('form' , {value:'<script>consloe.log("x")</script>'})
+    res.render('form' , {value:''})
 })
+
+// app.get('/assets/RMUTT.png' , (req , res)=>{
+//     fs.readFileSync('assets/RMUTT.png')
+// })
 
 app.listen(port , ()=>{
     console.log(`PORT ${port}`)
