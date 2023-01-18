c9b6bc656ed421696acccc55a81049c757f8964b
thanawat1303 <fquarius13@gmail.com>
2023-01-18 14:31:51 +0700

    complete

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express')
 const fs = require('fs')
 const app = express()
 let port = '8000'
+  
 
+app.use(express.json())
 app.set('view engine' , 'ejs')
 app.set('views' , 'view')
 app.use(express.static('assets'))
+app.use(express.static('view'))
 
 app.get('/' , (req , res)=>{
-    res.render('form' , {value:''})
+    res.render('form' , {value:''} , (err , html)=>{
+        res.send(html)
+    })
+})
+
+app.post('/profile' , (req , res)=>{
+    res.status(200).render('profile' , {value:''})
 })
 
 // app.get('/assets/RMUTT.png' , (req , res)=>{
